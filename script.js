bfe651672a78a48898678dea769243f9b1afcb4f
jhervhyce <jhervhyce.concepcion@gmail.com>
2025-01-09 10:53:19 -1000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+window.onload = function() {
+
+  function displayTime(){
+
+    var currentTime = new Date();
+
+    var hours = currentTime.getHours();
+
+    var minutes = currentTime.getMinutes();
+
+    var seconds = currentTime.getSeconds();
+
+    var clockDiv = document.getElementById('clock');
+
+        if (seconds < 10) {
+          seconds = "0" + seconds;
+        }
+       if (minutes < 10) {
+          minutes = "0" + minutes;
+        }
+
+    var meridiem = "AM";
+
+    if (hours > 12) {
+    hours = hours - 12;
+    meridiem = "PM";
+    }
+    if (hours === 0) {
+    hours = 12;    
+    }
+
+    clockDiv.innerText = hours + ":" + minutes + ":" + seconds + " " + meridiem;
+
+  }
+
+  displayTime();
+
+    setInterval(displayTime, 1000);
+  
+
+}
+
